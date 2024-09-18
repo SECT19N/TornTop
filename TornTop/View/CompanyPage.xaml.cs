24c665b694dcbeffc6f00bb7ae5b3063e1d5e30c
alex mason <alexmas9n@gmail.com>
2024-09-18 09:34:38 +0300

    item description to item panel

--- TornTop/View/CompanyPage.xaml.cs
+++ TornTop/View/CompanyPage.xaml.cs
@@ -1,22 +1,80 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using TornAPI;
+using TornAPI.CompanyData;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Newtonsoft.Json;
+using System.Net.Http;
+using TornTop.Model;
+using Windows.Storage;
+using System.Threading.Tasks;
+using TornAPI.Enums;
+using TornAPI.TornData;
 
 namespace TornTop.View;
 
 public sealed partial class CompanyPage : Page {
+	Client Client { get; set; }
+	Company Company { get; set; }
+
 	public CompanyPage() {
 		this.InitializeComponent();
+	}
+
+	private async void Page_Loaded(object sender, RoutedEventArgs e) {
+		try {
+			StorageFile settingsFile = await ApplicationData.Current.RoamingFolder.GetItemAsync("Settings.json") as StorageFile;
+
+			string json = await FileIO.ReadTextAsync(settingsFile);
+			Settings settings = JsonConvert.DeserializeObject<Settings>(json);
+
+			Client = new(settings.ApiKey);
+
+			await GetDataAsync();
+		} catch (HttpRequestException ex) {
+			ContentDialog dialog = new() {
+				Title = "Host Not Found",
+				Content = "Can't connect to API Host, Message: " + ex.Message,
+				XamlRoot = this.Content.XamlRoot,
+				CloseButtonText = "OK"
+			};
+
+			await dialog.ShowAsync();
+		} catch (Exception ex) {
+			ContentDialog dialog = new() {
+				Title = "Error",
+				Content = ex.Message,
+				XamlRoot = this.Content.XamlRoot,
+				CloseButtonText = "OK"
+			};
+
+			await dialog.ShowAsync();
+		}
+	}
+
+	async Task GetDataAsync() {
+		try {
+			Company = await Client.GetCompanyAsync(CompanySelections.Applications);
+
+			EmployeesListView.ItemsSource = Company.Employees.Values;
+		} catch (HttpRequestException ex) {
+			ContentDialog dialog = new() {
+				Title = "Host Not Found",
+				Content = "Can't connect to API Host, Message: " + ex.Message,
+				XamlRoot = this.Content.XamlRoot,
+				CloseButtonText = "OK"
+			};
+
+			await dialog.ShowAsync();
+		} catch (Exception ex) {
+			ContentDialog dialog = new() {
+				Title = "Error",
+				Content = ex.Message,
+				XamlRoot = this.Content.XamlRoot,
+				CloseButtonText = "OK"
+			};
+
+			await dialog.ShowAsync();
+		}
 	}
 }
