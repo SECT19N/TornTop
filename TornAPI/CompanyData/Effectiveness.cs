2a92fb32b4bdbe26773e45c4882957abf57daac7
alex mason <alexmas9n@gmail.com>
2024-09-24 14:45:39 +0300

    added entire effectiveness properties

--- TornAPI/CompanyData/Effectiveness.cs
+++ TornAPI/CompanyData/Effectiveness.cs
@@ -4,17 +4,23 @@ namespace TornAPI.CompanyData;
 
 public class Effectiveness {
 	[JsonProperty("working_stats")]
-	public int WorkingStats { get; set; }
+	public int WorkingStats { get; set; } = 0;
 
 	[JsonProperty("settled_in")]
-	public int SettledIn { get; set; }
+	public int SettledIn { get; set; } = 0;
 
 	[JsonProperty("director_education")]
-	public int DirectorEducation { get; set; }
+	public int DirectorEducation { get; set; } = 0;
+
+	[JsonProperty("management")]
+	public int Management { get; set; } = 0;
 
 	[JsonProperty("addiction")]
-	public int Addiction { get; set; }
+	public int Addiction { get; set; } = 0;
+
+	[JsonProperty("inactivity")]
+	public int Inactivity { get; set; } = 0;
 
 	[JsonProperty("total")]
-	public int Total { get; set; }
+	public int Total { get; set; } = 0;
 }
