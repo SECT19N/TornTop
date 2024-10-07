a55fdd4ca0a379081c99ad97ab5d22852cd8edff
alex mason <alexmas9n@gmail.com>
2024-10-07 12:44:13 +0300

    converting status

---
+++ TornTop/Model/Converters/StatusToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace TornTop.Model.Converters;
+
+class StatusToColorConverter : IValueConverter {
+	public object Convert(object value, Type targetType, object parameter, string language) {
+		return value.ToString().ToLower() switch {
+			"online" => "#6cad2b",
+			"idle" => "#cccc32",
+			"offline" => "#555",
+			_ => "#555",
+		};
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language) {
+		throw new NotImplementedException();
+	}
+}
